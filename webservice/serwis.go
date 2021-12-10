2d9bfac0e38ba267dfcdfbe5373e95ae36defdd2
tomaszydlo2 <tom.ginszt@gmail.com>
2021-12-10 14:31:59 +0100

    backup 10.12 friday

--- webservice/serwis.go
+++ webservice/serwis.go
@@ -1,38 +1,56 @@
 package main
 
 import (
+    "database/sql"
     "encoding/json"
     "fmt"
-    "database/sql"
-    "os"
+    _ "github.com/mattn/go-sqlite3"
     "log"
     "net/http"
-    _ "github.com/mattn/go-sqlite3"
+    "os"
 )
 
+// struktura będąca odpowiednikiem do struktury bazy danych
 type ziolaStruct struct {
     Nazwa string
     Dzialanie string
     Wystepowanie string
 }
+/*
+TODO:
+1.
+- (^>^) GET zwraca liste rzeczy z DB
+- Zrob nowy package, z funkcjami dotyczącymi DB- np. db.go. Zaimportuj go i używaj.
+- Implementacja endpoitów osobnych funkcjach, nie w mainie
+- Dockerfile. Musi być możliwość zrobienia `docker run .` w folderze webservice, i serwis ma działać.
+2.
+- Zmiana bazy danych na postgresql (Dockerfile przestanie dzialac)
+- Docker-compose uzywajacy istniejacego Dockerfile (ma znow dzialac)
 
+
+*/
 func main() {
-    os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records. SQLite is a file based database.
+    // czyścimy baze danych żeby było wszystko widać
+    os.Remove("sqlite-database.db")
+
+    // tworzymy baze danych
     log.Println("Creating sqlite-database.db...")
     file, err := os.Create("sqlite-database.db") // Create SQLite file
         checkErr(err)
-
     file.Close()
     log.Println("sqlite-database.db created")
 
-    sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
-    defer sqliteDatabase.Close() // Defer Closing the database
+    // otwarcie i zdeferowane zamknięcie pliku z baną dazych
+    sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db")
+    defer sqliteDatabase.Close()
 
-    createTable(sqliteDatabase) // Create Database Table
+    // tworzymy strukturę bazy (tabelki)
+    createTable(sqliteDatabase)
 
     // post
     postHandler := func(w http.ResponseWriter, req *http.Request) {
 
+        // czytanie jsona do struktury ziolaS
         decoder := json.NewDecoder(req.Body)
         var ziolaS ziolaStruct
         err := decoder.Decode(&ziolaS)
@@ -41,19 +59,19 @@ func main() {
         // INSERT RECORDS
         log.Println("Uwaga, dodano -> Nazwa: ", ziolaS.Nazwa," Dzialanie: ", ziolaS.Dzialanie," Wystepowanie: ", ziolaS.Wystepowanie)
         insertIntoTable(sqliteDatabase, ziolaS.Nazwa, ziolaS.Dzialanie, ziolaS.Wystepowanie)
-
-        return 1
     }
+
     // get
     getHandler := func(w http.ResponseWriter, req *http.Request) {
+
         // ładnie wyświetla select * from database
-        printFromTable(sqliteDatabase)
+        fmt.Fprintf(w,printFromTable(sqliteDatabase))
     }
 
+    // poniższe funkcje stawiają nam serwer
     http.HandleFunc("/post", postHandler)
     http.HandleFunc("/get", getHandler)
     log.Fatal(http.ListenAndServe(":1234", nil))
-
 }
 
 func createTable(db *sql.DB) {
@@ -71,6 +89,7 @@ func createTable(db *sql.DB) {
 
     defer statement.Close()
 
+    //wywołanie kwerendy
     _, err = statement.Exec()
         checkErr(err)
 }
@@ -85,26 +104,35 @@ func insertIntoTable(db *sql.DB, nazwa string, dzialanie string, wystepowanie st
 
     defer statement.Close()
 
+    // wywołanie kwerendy
     _, err = statement.Exec(nazwa, dzialanie, wystepowanie)
         checkErr(err)
 
 }
 
-func printFromTable(db *sql.DB) {
+func printFromTable(db *sql.DB) string {
+    //pobieramy po rzędzie dane bazych
     row, err := db.Query(fmt.Sprintf("SELECT * FROM ziola"))
         checkErr(err)
     defer row.Close()
 
+    // do tego stringa zwracamy wszystkie dane ładnie ułożone
+    var fullReturn string
+
+    // czytamy po rzędzie z bazy, wrzucamy wartości do fullReturn
     for row.Next() {
         var id int
         var nazwa string
         var dzialanie string
         var wystepowanie string
         row.Scan(&id, &nazwa, &dzialanie, &wystepowanie)
-        log.Println("Ziolo: ", nazwa, " ", dzialanie, " ", wystepowanie)
+
+        fullReturn += "Ziolo: " + nazwa + " " + dzialanie + " " + wystepowanie +"\n"
     }
+    return fullReturn
 }
 
+//Sprawdzanie errorów nigdy jeszcze nie było tak szybkie i proste ;)
 func checkErr(err error){
     if err != nil {
         log.Fatalln(err.Error())
